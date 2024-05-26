bba6f139633a74e6cce8d7ee17fca26ed8cafe21
Ahmad Shamsi <ahmaddshamsii@gmail.com>
2024-05-26 20:58:58 +0500

    created responsive layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,14 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import './global.css'
+import "./global.css";
 import { Anchor, Col, Row } from "antd";
 import { useMediaQuery } from "react-responsive";
+import MobileLayout from "./components/MobileLayout";
+import DesktopLayout from "./components/DesktopLayout";
 
 const Home = () => {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
-  return (
-    <Row>
-      <Col span={4}>
-        <Anchor
-          items={[
-            {
-              key: "part-1",
-              href: "#part-1",
-              title: "Part 1",
-            },
-            {
-              key: "part-2",
-              href: "#part-2",
-              title: "Part 2",
-            },
-            {
-              key: "part-3",
-              href: "#part-3",
-              title: "Part 3",
-            },
-          ]}
-        />
-      </Col>
-      <Col span={20}>
-        <div
-          id="part-1"
-          style={{ height: "100vh", background: "blue" }}
-        />
-        <div
-          id="part-2"
-          style={{ height: "100vh", background: "green" }}
-        />
-        <div
-          id="part-3"
-          style={{ height: "100vh", background: "red" }}
-        />
-      </Col>
-    </Row>
-  );
+  return <div>{isSmallScreen ? <MobileLayout /> : <DesktopLayout />}</div>;
 };
 export default Home;
