d1e1a531904fe459b2f54da0fb884e4dc460d33d
Ahmad Shamsi <ahmaddshamsii@gmail.com>
2024-06-18 11:50:11 +0500

    fixed layout issues

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import MobileLayout from "@/components/MobileLayout";
+import DesktopLayout from "@/components/DesktopLayout";
+import "./../../app/global.css";
+import { isMobileView } from "@/utils/constants";
+
+const Layout = ({ children }:any) => {
+  return (
+    <div>
+      {/* {isMobileView ? (
+        <MobileLayout children={children} />
+      ) : (
+        <DesktopLayout children={children} />
+      )} */}
+    </div>
+  );
+};
+
+export default Layout;
