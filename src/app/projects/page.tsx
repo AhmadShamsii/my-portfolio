7efbcd2580b3d2a3a48635e880758452a5a1e815
Ahmad Shamsi <ahmaddshamsii@gmail.com>
2025-01-29 13:09:42 +0500

    enabled email configuration

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,28 @@
+"use client"
+import { Col, Row } from "antd";
+import { StyledText } from "../page";
+import { cardData } from "@/utils/constants";
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+
 const Projects = () => {
   return (
-    <div style={{ height: "100vh", background: "green" }}>
-      This is the Projects page content.
+    <div>
+      <StyledText style={{ marginBottom: "30px" }}>Projects</StyledText>
+      <Row style={{ margin: "0 10vw" }} gutter={[24, 24]}>
+        {cardData?.map((data, index) => (
+          <Col key={`col-${index}`} span={data?.span}>
+            <ProjectCard
+              imageSrc={data.imageSrc}
+              imageAltText={data.imageAltText}
+              projectTitle={data.projectTitle}
+              projectDesc={data.projectDesc}
+              projectYear={data.projectYear}
+              tags={data.tags}
+              index={index}
+            />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,51 +1,48 @@
-import React, { useState } from "react";
-import { Form, Input, Button } from "antd";
-import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import { Form, Input, Button, message } from "antd";
 import emailjs from "emailjs-com";
 import TextArea from "antd/es/input/TextArea";
 import { ArrowRightOutlined, GithubOutlined, InstagramOutlined, LinkedinOutlined } from "@ant-design/icons";
-import Text from "antd/es/typography/Text";
-import Title from "antd/es/typography/Title";
 import { StyledText, StyledTitle } from "./styles";
-
 
 const ContactForm: React.FC = () => {
     const [form] = Form.useForm();
 
     const onFinish = (values: any) => {
         emailjs
-            .send("your_service_id", "your_template_id", values, "your_public_key")
+            .send(
+                process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID as string,
+                process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID as string,
+                values,
+                process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY as string
+            )
             .then(() => {
                 form.resetFields();
-                alert("Message sent successfully!");
+                message.success("Message sent successfully!");
             })
-            .catch(() => alert("Failed to send message!"));
+            .catch((err) => message.error("Error sending message!"));
     };
 
     return (
-        <div style={{ display: "flex", justifyContent: "center", gap: "5vw", }} >
-            <div style={{ marginTop: "2%" }} >
-                <StyledTitle >
-                    Let's Talk
-                </StyledTitle>
-                <StyledText style={{ fontSize: "14px", color: "gray" }}> I am based in <strong> Islamabad, Pakistan.</strong> <br />
+        <div style={{ display: "flex", justifyContent: "center", gap: "5vw" }}>
+            <div style={{ marginTop: "2%" }}>
+                <StyledTitle>Let's Talk</StyledTitle>
+                <StyledText style={{ fontSize: "14px", color: "gray" }}>
+                    I am based in <strong>Islamabad, Pakistan.</strong> <br />
                     You can contact me via form or mail me directly
                 </StyledText>
 
-                <StyledTitle style={{ fontSize: "16px" }}>
-                    Email
-                </StyledTitle>
-                <StyledText>ahmaddshamsii@gmail.com
-                </StyledText>
-                <StyledTitle style={{ fontSize: "16px" }}>
-                    Connect
-                </StyledTitle>
+                <StyledTitle style={{ fontSize: "16px" }}>Email</StyledTitle>
+                <StyledText>ahmaddshamsii@gmail.com</StyledText>
+
+                <StyledTitle style={{ fontSize: "16px" }}>Connect</StyledTitle>
                 <StyledText>
                     <GithubOutlined /> <LinkedinOutlined /> <InstagramOutlined />
                 </StyledText>
             </div>
+
             <Form
+                form={form}
                 name="basic"
                 onFinish={onFinish}
                 layout="vertical"
@@ -55,7 +52,7 @@ const ContactForm: React.FC = () => {
                 <Form.Item
                     label={<StyledTitle>Name</StyledTitle>}
                     name="name"
-                    rules={[{ required: true, message: 'Please enter your name!' }]}
+                    rules={[{ required: true, message: "Please enter your name!" }]}
                 >
                     <Input size="small" variant="borderless" style={{ borderBottom: "1px solid gray" }} />
                 </Form.Item>
@@ -63,7 +60,7 @@ const ContactForm: React.FC = () => {
                 <Form.Item
                     label={<StyledTitle>Subject</StyledTitle>}
                     name="subject"
-                    rules={[{ required: true, message: 'Please enter your subject!' }]}
+                    rules={[{ required: true, message: "Please enter your subject!" }]}
                 >
                     <Input size="small" variant="borderless" style={{ borderBottom: "1px solid gray" }} />
                 </Form.Item>
@@ -71,13 +68,19 @@ const ContactForm: React.FC = () => {
                 <Form.Item
                     label={<StyledTitle>Message</StyledTitle>}
                     name="message"
-                    rules={[{ required: true, message: 'Please enter your message!' }]}
+                    rules={[{ required: true, message: "Please enter your message!" }]}
                 >
                     <TextArea size="small" variant="borderless" style={{ borderBottom: "1px solid gray" }} rows={4} />
                 </Form.Item>
 
                 <Form.Item style={{ display: "flex", justifyContent: "end" }} label={null}>
-                    <Button type="text" style={{ borderBottom: "1px solid gray" }} iconPosition="end" icon={<ArrowRightOutlined />} htmlType="submit">
+                    <Button
+                        type="text"
+                        style={{ borderBottom: "1px solid gray" }}
+                        iconPosition="end"
+                        icon={<ArrowRightOutlined />}
+                        htmlType="submit"
+                    >
                         Send
                     </Button>
                 </Form.Item>
